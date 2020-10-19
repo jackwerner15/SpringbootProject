4439669da79ba3526aad46df0ad4ba72e866b6ed
Jack Werner <jwerner@Jacks-MacBook-Pro-2.local>
2020-10-19 12:13:30 -0400

    imported the ui model

--- src/main/java/com/tts/subscriberlist/subscriber/SubscriberController.java
+++ src/main/java/com/tts/subscriberlist/subscriber/SubscriberController.java
@@ -2,6 +2,7 @@ package com.tts.subscriberlist.subscriber;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
